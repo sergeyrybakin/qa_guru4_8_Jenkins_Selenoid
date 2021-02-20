9cd207a4b4e6884cae136cf6ed25b4a8ba4bfda0
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-02-20 22:57:16 +0700

    Add negative test

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,18 @@
 package tests;
 
+import io.qameta.allure.selenide.AllureSelenide;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.codeborne.selenide.Configuration;
 
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+
 public class TestBase {
     @BeforeAll
     static void setup() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
         Configuration.startMaximized = true;
 
         //config for Java + Selenide
