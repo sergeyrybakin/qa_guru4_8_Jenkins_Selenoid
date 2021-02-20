549941f1bff8ff252a535442522251609df83039
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-02-20 23:16:00 +0700

    Add Allure for Steps and Page Object

--- src/test/java/tests/StudentRegistrationFormWithPageObjectsTests.java
+++ src/test/java/tests/StudentRegistrationFormWithPageObjectsTests.java
@@ -1,13 +1,11 @@
 package tests;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import pages.StudentRegistrationPage;
 
 class StudentRegistrationFormWithPageObjectsTests extends TestBase {
     StudentRegistrationPage studentRegistrationPage;
 
-    @Disabled
     @Test
     void successfulFillFormTest() {
         studentRegistrationPage = new StudentRegistrationPage();
