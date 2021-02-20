6389e6a7b1244ca35a34f441bc3fcdd536813c1e
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-02-20 21:20:16 +0700

    Add selenoid driver

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.codeborne.selenide.Configuration;
 
@@ -8,5 +9,11 @@ public class TestBase {
     @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
+
+        //config for Java + Selenide
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("enableVNC", true);
+//        capabilities.setCapability("enableVideo", true);
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub";
     }
 }
